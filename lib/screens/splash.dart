e06f4bfd3de57d34022844df986b6d94e0846dbc
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2026-01-15 03:53:08 -0800

    feat: Done with the splash

---
+++ lib/screens/splash.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _bigLogoFade;
+  late Animation<Offset> _slide;
+  late Animation<Offset> _slideText;
+  late Animation<double> _textFade;
+  late Animation<double> _imageScale;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2200),
+    );
+
+    _bigLogoFade = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.2, 0.8, curve: Curves.easeIn),
+      ),
+    );
+
+    _imageScale = Tween<double>(begin: 0.85, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.2, 0.8, curve: Curves.easeOutBack),
+      ),
+    );
+
+    _slide = Tween<Offset>(begin: const Offset(0, 0.3), end: Offset.zero)
+        .animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: const Interval(0.2, 0.8, curve: Curves.easeOut),
+          ),
+        );
+
+    _slideText = Tween<Offset>(begin: const Offset(0, 0.3), end: Offset.zero)
+        .animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: const Interval(0.6, 1.0, curve: Curves.easeOut),
+          ),
+        );
+
+    _textFade = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.6, 1.0, curve: Curves.easeIn),
+      ),
+    );
+
+    _controller.forward();
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        if (!mounted) return;
+        Get.offNamed('/login');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeContext = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: themeContext.colorScheme.primary,
+      body: Center(
+        child: SlideTransition(
+          position: _slide,
+          child: FadeTransition(
+            opacity: _bigLogoFade,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ScaleTransition(
+                  scale: _imageScale,
+                  child: Image.asset(
+                    'assets/images/splashlogo.png',
+                    height: 100.h,
+                  ),
+                ),
+                20.verticalSpace,
+                SlideTransition(
+                  position: _slideText,
+                  child: FadeTransition(
+                    opacity: _textFade,
+                    child: Column(
+                      children: [
+                        Text(
+                          "BillFlow",
+                            style: themeContext.textTheme.headlineLarge?.copyWith(
+                                color: themeContext.colorScheme.onPrimary),
+                         
+                        ),
+                        SizedBox(height: 8.h),
+                        Text(
+                          "Your Smart Bill Tracking and Reminder App",
+                          style: themeContext.textTheme.bodyLarge?.copyWith(color: themeContext.colorScheme.onPrimaryContainer),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/route.dart
@@ -0,0 +1,24 @@
+import 'package:billflow/screens/auth/login.dart';
+import 'package:billflow/screens/auth/register.dart';
+import 'package:billflow/screens/auth/verify_email.dart';
+import 'package:billflow/screens/splash.dart';
+import 'package:flutter/material.dart';
+
+class OnGenerateRoute {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      case "/auth/login":
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case "/auth/register":
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+
+      case "/auth/verify-mail":
+        return MaterialPageRoute(builder: (_) => const VerifyEmailScreen());
+      default:
+        return MaterialPageRoute(builder: (_) => Container());
+    }
+  }
+}
